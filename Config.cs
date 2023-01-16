342a3967d365892f6f11c37f11ecf8ef804ae792
BlackoutRoulette <blackoutroulette@live.de>
2023-01-16 17:00:29 +0100

    Added config file and template text option

---
+++ Config.cs
@@ -0,0 +1,72 @@
+﻿namespace DSDeaths
+{
+    public class Config
+    {
+        public static readonly string json =
+@"{
+  ""Dark Souls I"": {
+    ""process"": ""DARKSOULS"",
+    ""offsets"": [
+      ""0xF78700"",
+      ""0x5c""
+    ],
+    ""isWow64"": true
+  },
+  ""Dark Souls II"": {
+    ""process"": ""DarkSoulsII"",
+    ""offsets"": [
+      ""0x1150414"",
+      ""0x74"",
+      ""0xB8"",
+      ""0x34"",
+      ""0x4"",
+      ""0x28C"",
+      ""0x100""
+    ],
+    ""isWow64"": true
+  },
+  ""Dark Souls II: Scholar of the First Sin"": {
+    ""process"": ""DarkSoulsII"",
+    ""offsets"": [
+      ""0x16148F0"",
+      ""0xD0"",
+      ""0x490"",
+      ""0x104""
+    ],
+    ""isWow64"": false
+  },
+  ""Dark Souls III"": {
+    ""process"": ""DarkSoulsIII"",
+    ""offsets"": [
+      ""0x47572B8"",
+      ""0x98""
+    ],
+    ""isWow64"": false
+  },
+  ""Dark Souls I: Remastered"": {
+    ""process"": ""DarkSoulsRemastered"",
+    ""offsets"": [
+      ""0x1C8A530"",
+      ""0x98""
+    ],
+    ""isWow64"": false
+  },
+  ""Sekiro"": {
+    ""process"": ""Sekiro"",
+    ""offsets"": [
+      ""0x3D5AAC0"",
+      ""0x90""
+    ],
+    ""isWow64"": false
+  },
+  ""Elden Ring"": {
+    ""process"": ""eldenring"",
+    ""offsets"": [
+      ""0x3C17EE8"",
+      ""0x94""
+    ],
+    ""isWow64"": false
+  }
+}";
+    }
+}
