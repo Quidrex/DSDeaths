014cd2216e9a0adf6b0f97abdefe00fe0e568cf5
Hue <hue.dynamics@gmail.com>
2023-09-17 03:52:39 -0400

    Adding Sammi Calls
    + Added an app config that allows one to specify properties of SAMMI integration
    + Added Costura so single-exe-publishing can be done
    + Added SAMMI Api ref

--- Program.cs
+++ Program.cs
@@ -1,24 +1,31 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Diagnostics;
 using System.IO;
+using System.Net.Http;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
-
-namespace DSDeaths {
-    class Game {
+namespace DSDeaths
+{
+    class Game
+    {
         public readonly string name;
         public readonly int[] offsets32;
         public readonly int[] offsets64;
 
-        public Game(in string name, in int[] offsets32, in int[] offsets64) {
+        public Game(in string name, in int[] offsets32, in int[] offsets64)
+        {
             this.name = name;
             this.offsets32 = offsets32;
             this.offsets64 = offsets64;
         }
     }
 
-    class Program {
+    class Program
+    {
         const int PROCESS_WM_READ = 0x0010;
         const int PROCESS_QUERY_INFORMATION = 0x0400;
 
@@ -42,30 +49,74 @@ namespace DSDeaths {
             new Game("eldenring", null, new int[] {0x3CD4D88, 0x94})
         };
 
-        static bool Write(int value) {
-            try {
-                File.WriteAllText("DSDeaths.txt", value.ToString());
-            } catch (IOException) {
+        static bool Write(int value)
+        {
+            try
+            {
+                File.WriteAllText(Properties.Settings.Default.FileName, value.ToString());
+                if (Properties.Settings.Default.EnableSammi)
+                {
+                    //Trigger SAMMI Button
+                    SignalSAMMIAsync(value);
+                }
+            }
+            catch (IOException)
+            {
                 Console.WriteLine("Could not write to DSDeaths.txt.");
                 return false;
             }
             return true;
         }
 
-        static bool PeekMemory(in IntPtr handle, in IntPtr baseAddress, bool isX64, in int[] offsets, ref int value) {
+        static async Task SignalSAMMIAsync(int value = 0)
+        {
+            System.Net.ServicePointManager.Expect100Continue = false;
+            HttpClient client = new HttpClient();
+            string json = string.Empty;
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+            if (string.IsNullOrWhiteSpace(Properties.Settings.Default.SammiVariableName) == false)
+            {
+                json = JsonConvert.SerializeObject(new
+                {
+                    request = "setVariable",
+                    name = Properties.Settings.Default.SammiVariableName,
+                    value = value.ToString(),
+                    buttonID = Properties.Settings.Default.SammiButtonID
+                });
+                httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+                await client.PostAsync(Properties.Settings.Default.SammiUrl, httpContent);
+            }
+
+            if (string.IsNullOrWhiteSpace(Properties.Settings.Default.SammiButtonID) == false)
+            {
+                json = JsonConvert.SerializeObject(new
+                {
+                    request = "triggerButton",
+                    buttonID = Properties.Settings.Default.SammiButtonID
+                });
+                httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+                await client.PostAsync(Properties.Settings.Default.SammiUrl, httpContent);
+            }
+        }
+
+        static bool PeekMemory(in IntPtr handle, in IntPtr baseAddress, bool isX64, in int[] offsets, ref int value)
+        {
             long address = baseAddress.ToInt64();
             byte[] buffer = new byte[8];
             int discard = 0;
 
-            foreach (int offset in offsets) {
-                if (address == 0) {
+            foreach (int offset in offsets)
+            {
+                if (address == 0)
+                {
                     Console.WriteLine("Encountered null pointer.");
                     return false;
                 }
 
                 address += offset;
 
-                if (!ReadProcessMemory(handle, (IntPtr)address, buffer, 8, ref discard)) {
+                if (!ReadProcessMemory(handle, (IntPtr)address, buffer, 8, ref discard))
+                {
                     Console.WriteLine("Could not read game memory.");
                     return false;
                 }
@@ -77,12 +128,13 @@ namespace DSDeaths {
             return true;
         }
 
-
-
-        static bool ScanProcesses(ref Process proc, ref Game game) {
-            foreach (Game g in games) {
+        static bool ScanProcesses(ref Process proc, ref Game game)
+        {
+            foreach (Game g in games)
+            {
                 Process[] process = Process.GetProcessesByName(g.name);
-                if (process.Length != 0) {
+                if (process.Length != 0)
+                {
                     Console.WriteLine("Found: " + g.name);
                     proc = process[0];
                     game = g;
@@ -92,8 +144,11 @@ namespace DSDeaths {
             return false;
         }
 
-        static void Main() {
-            Console.CancelKeyPress += delegate {
+        static void Main()
+        {
+            Console.WriteLine($"Application Configuration is located in {AppDomain.CurrentDomain.SetupInformation.ConfigurationFile}");
+            Console.CancelKeyPress += delegate
+            {
                 Write(0);
             };
 
@@ -104,14 +159,16 @@ namespace DSDeaths {
             Console.WriteLine("-----------------------------------WARNING-----------------------------------");
             Console.WriteLine();
 
-            while (true) {
+            while (true)
+            {
                 Write(0);
                 Console.WriteLine("Looking for Dark Souls process...");
 
                 Process proc = null;
                 Game game = null;
 
-                while (!ScanProcesses(ref proc, ref game)) {
+                while (!ScanProcesses(ref proc, ref game))
+                {
                     Thread.Sleep(500);
                 }
 
@@ -120,13 +177,17 @@ namespace DSDeaths {
                 int oldValue = 0, value = 0;
 
                 bool isWow64 = false;
-                if (IsWow64Process(handle, ref isWow64)) {
+                if (IsWow64Process(handle, ref isWow64))
+                {
                     Console.WriteLine("Found " + (isWow64 ? "32" : "64") + " bit variant.");
                     int[] offsets = isWow64 ? game.offsets32 : game.offsets64;
 
-                    while (!proc.HasExited) {
-                        if (PeekMemory(handle, baseAddress, !isWow64, offsets, ref value)) {
-                            if (value != oldValue) {
+                    while (!proc.HasExited)
+                    {
+                        if (PeekMemory(handle, baseAddress, !isWow64, offsets, ref value))
+                        {
+                            if (value != oldValue)
+                            {
                                 oldValue = value;
                                 Write(value);
                             }
